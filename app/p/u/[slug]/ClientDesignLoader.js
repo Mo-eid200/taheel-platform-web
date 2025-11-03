0cba9578a5fec4dcd818f62a0733ae2929ab6069
Mo-eid200 <mo.atta.eid@gmail.com>
2025-11-03 15:45:43 +0400

    14

--- app/p/u/[slug]/ClientDesignLoader.js
+++ app/p/u/[slug]/ClientDesignLoader.js
@@ -5,7 +5,7 @@ export default function ClientDesignLoader({ slug }) {
   const key = String(slug || "").toLowerCase();
 
   // نحاول نحمل أي ملف في _designs بنفس الاسم
-  const Design = dynamic(() => import(`../../_designs/${key}`), { ssr: false });
+  const Design = dynamic(() => import(`../_designs/${key}`), { ssr: false });
 
   return <Design />;
 }
